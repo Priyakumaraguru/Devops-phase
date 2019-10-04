053db8d431cc74a9f889b5047bc7fb9a8c3e46c2
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-10-04 15:36:15 +0530

    Update countATest.java

--- src/test/java/com/maven/test/countATest.java
+++ src/test/java/com/maven/test/countATest.java
@@ -9,7 +9,7 @@ public class countATest {
 	@Test
 	public void test() {
 		JunitTesting test = new JunitTesting();
-		int output= test.countA("CodeKillers");
+		int output= test.count("CodeKillers");
 		assertEquals(11,output);
 	}
 
