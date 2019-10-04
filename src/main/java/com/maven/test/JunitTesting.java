21bc87d677e5f8254a2c4b5fb53d680e07e4b0d3
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-10-04 15:30:43 +0530

    Update JunitTesting.java

--- src/main/java/com/maven/test/JunitTesting.java
+++ src/main/java/com/maven/test/JunitTesting.java
@@ -1,19 +1,13 @@
 package com.maven.test;
 
 public class JunitTesting {
-        public int square(int x)
+        public double power(double x,double y)
         {
-        	return x*x;
+        	return Math.pow(x,y);
         }
         public int countA(String word)
         {
-        	int count=0;
-        	for(int i=0;i<word.length();i++){
-        		if(word.charAt(i)=='a' || word.charAt(i)=='A')
-        		{
-        			count++;
-        		}
-        	}
-        	return count;
+        
+        	return word.length();
         }
 }
